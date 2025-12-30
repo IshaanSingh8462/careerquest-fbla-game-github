63891fb4682b559d0eeeaed7a0b31567cea9e9fb
IshaanSingh8462 <ishaan8462@gmail.com>
2025-12-29 19:21:49 -0500

    electrician fuses trip
    added color to breaker to indicate if switches have been tripped or not

--- careerquest/scripts/player_test.gd
+++ careerquest/scripts/player_test.gd
@@ -163,7 +163,7 @@ func _physics_process(delta: float) -> void:
 					for i in range(10):
 						if object.is_in_group(sw[i]):
 							electric.switches_move(i+1)
-							Global.sw_num(i)
+							Global.sw_num(i) 
 		if object.is_in_group("outlet_scene1") and Global.is_elec:
 			if !object.has_method("interact"):
 				label.show()
